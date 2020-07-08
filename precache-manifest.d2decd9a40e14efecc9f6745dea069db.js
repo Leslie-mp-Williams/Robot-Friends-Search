2ce45da8307efa789d0d16a89be10a62258f3941
Leslie-mp-Williams <clarkie637@gmail.com>
2020-07-08 11:58:49 +0100

    Updates

---
+++ precache-manifest.d2decd9a40e14efecc9f6745dea069db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de18d916516b5162e3708c1ceb72709b",
+    "url": "/Robot-Friends-Search/index.html"
+  },
+  {
+    "revision": "1dc1f44c5ef66dda74a0",
+    "url": "/Robot-Friends-Search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9aef49adfe3bbbae5b26",
+    "url": "/Robot-Friends-Search/static/css/main.211a9ecf.chunk.css"
+  },
+  {
+    "revision": "1dc1f44c5ef66dda74a0",
+    "url": "/Robot-Friends-Search/static/js/2.359e551c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robot-Friends-Search/static/js/2.359e551c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aef49adfe3bbbae5b26",
+    "url": "/Robot-Friends-Search/static/js/main.61b56c68.chunk.js"
+  },
+  {
+    "revision": "5f50cd04b3d26774b645",
+    "url": "/Robot-Friends-Search/static/js/runtime-main.9bb8606b.js"
+  },
+  {
+    "revision": "7cc222a3be796abb86c6308b5862ff59",
+    "url": "/Robot-Friends-Search/static/media/Urba.7cc222a3.woff"
+  }
+]);
